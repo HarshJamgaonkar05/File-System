61f362346d4c72b125f14c2817c51c1561de4d13
HarshJamgaonkar05 <harshnj.1209@gmail.com>
2025-11-03 07:20:39 +0000

    code

--- simplefs-ops.c
+++ simplefs-ops.c
@@ -5,7 +5,33 @@ int simplefs_create(char *filename){
     /*
 	    Create file with name `filename` from disk
 	*/
-    return -1;
+    if (strlen(filename) == 0 || strlen(filename) >= MAX_NAME_STRLEN) {
+        return -1;
+    }
+    
+    struct inode_t inode;
+    for (int i = 0; i < NUM_INODES; i++) {
+        simplefs_readInode(i, &inode);
+        if (inode.status == INODE_IN_USE && strcmp(inode.name, filename) == 0) {
+            return -1;
+        }
+    }
+    
+    int inode_num = simplefs_allocInode();
+    if (inode_num == -1) {
+        return -1;
+    }
+    
+    simplefs_readInode(inode_num, &inode);
+    inode.status = INODE_IN_USE;
+    strcpy(inode.name, filename);
+    inode.file_size = 0;
+    for (int i = 0; i < MAX_FILE_SIZE; i++) {
+        inode.direct_blocks[i] = -1;
+    }
+    simplefs_writeInode(inode_num, &inode);
+    
+    return inode_num;
 }
 
 
@@ -13,27 +39,124 @@ void simplefs_delete(char *filename){
     /*
 	    delete file with name `filename` from disk
 	*/
+    struct inode_t inode;
+    int inode_num = -1;
+    
+    for (int i = 0; i < NUM_INODES; i++) {
+        simplefs_readInode(i, &inode);
+        if (inode.status == INODE_IN_USE && strcmp(inode.name, filename) == 0) {
+            inode_num = i;
+            break;
+        }
+    }
+    
+    if (inode_num == -1) {
+        return;
+    }
+    
+    for (int i = 0; i < MAX_FILE_SIZE; i++) {
+        if (inode.direct_blocks[i] != -1) {
+            simplefs_freeDataBlock(inode.direct_blocks[i]);
+        }
+    }
+    
+    simplefs_freeInode(inode_num);
 }
 
 int simplefs_open(char *filename){
     /*
 	    open file with name `filename`
 	*/
-    return -1;
+    struct inode_t inode;
+    int inode_num = -1;
+    
+    for (int i = 0; i < NUM_INODES; i++) {
+        simplefs_readInode(i, &inode);
+        if (inode.status == INODE_IN_USE && strcmp(inode.name, filename) == 0) {
+            inode_num = i;
+            break;
+        }
+    }
+    
+    if (inode_num == -1) {
+        return -1;
+    }
+    
+    int handle = -1;
+    for (int i = 0; i < MAX_OPEN_FILES; i++) {
+        if (file_handle_array[i].inode_number == -1) {
+            handle = i;
+            break;
+        }
+    }
+    
+    if (handle == -1) {
+        return -1;
+    }
+    
+    file_handle_array[handle].inode_number = inode_num;
+    file_handle_array[handle].offset = 0;
+    
+    return handle;
 }
 
 void simplefs_close(int file_handle){
     /*
 	    close file pointed by `file_handle`
 	*/
-
+    if (file_handle < 0 || file_handle >= MAX_OPEN_FILES) {
+        return;
+    }
+    
+    file_handle_array[file_handle].inode_number = -1;
+    file_handle_array[file_handle].offset = 0;
 }
 
 int simplefs_read(int file_handle, char *buf, int nbytes){
     /*
 	    read `nbytes` of data into `buf` from file pointed by `file_handle` starting at current offset
 	*/
-    return -1;
+    if (file_handle < 0 || file_handle >= MAX_OPEN_FILES || 
+        file_handle_array[file_handle].inode_number == -1) {
+        return -1;
+    }
+    
+    if (nbytes == 0) {
+        return 0;
+    }
+    
+    int inode_num = file_handle_array[file_handle].inode_number;
+    int offset = file_handle_array[file_handle].offset;
+    
+    struct inode_t inode;
+    simplefs_readInode(inode_num, &inode);
+    
+    if (offset + nbytes > inode.file_size) {
+        return -1;
+    }
+    
+    int bytes_read = 0;
+    while (bytes_read < nbytes) {
+        int current_offset = offset + bytes_read;
+        int block_index = current_offset / BLOCKSIZE;
+        int block_offset = current_offset % BLOCKSIZE;
+        int bytes_to_read = BLOCKSIZE - block_offset;
+        
+        if (bytes_to_read > nbytes - bytes_read) {
+            bytes_to_read = nbytes - bytes_read;
+        }
+        
+        if (block_index >= MAX_FILE_SIZE || inode.direct_blocks[block_index] == -1) {
+            return -1;
+        }
+        
+        char block_buf[BLOCKSIZE];
+        simplefs_readDataBlock(inode.direct_blocks[block_index], block_buf);
+        memcpy(buf + bytes_read, block_buf + block_offset, bytes_to_read);
+        bytes_read += bytes_to_read;
+    }
+    
+    return 0;
 }
 
 
@@ -41,7 +164,78 @@ int simplefs_write(int file_handle, char *buf, int nbytes){
     /*
 	    write `nbytes` of data from `buf` to file pointed by `file_handle` starting at current offset
 	*/
-    return -1;
+    if (file_handle < 0 || file_handle >= MAX_OPEN_FILES || 
+        file_handle_array[file_handle].inode_number == -1) {
+        return -1;
+    }
+    
+    if (nbytes == 0) {
+        return 0;
+    }
+    
+    int inode_num = file_handle_array[file_handle].inode_number;
+    int offset = file_handle_array[file_handle].offset;
+
+    struct inode_t inode;
+    simplefs_readInode(inode_num, &inode);
+
+    if (offset + nbytes > MAX_FILE_SIZE * BLOCKSIZE) {
+        return -1;
+    }
+    
+    int allocated_blocks[MAX_FILE_SIZE];
+    int num_allocated = 0;
+    for (int i = 0; i < MAX_FILE_SIZE; i++) {
+        allocated_blocks[i] = -1;
+    }
+    
+    int start_block = offset / BLOCKSIZE;
+    int end_block = (offset + nbytes - 1) / BLOCKSIZE;
+    
+    for (int i = start_block; i <= end_block; i++) {
+        if (inode.direct_blocks[i] == -1) {
+            int block_num = simplefs_allocDataBlock();
+            if (block_num == -1) {
+                for (int j = 0; j < num_allocated; j++) {
+                    simplefs_freeDataBlock(allocated_blocks[j]);
+                    inode.direct_blocks[start_block + j] = -1;
+                }
+                return -1;
+            }
+            inode.direct_blocks[i] = block_num;
+            allocated_blocks[num_allocated++] = block_num;
+            
+            char zero_buf[BLOCKSIZE];
+            memset(zero_buf, 0, BLOCKSIZE);
+            simplefs_writeDataBlock(block_num, zero_buf);
+        }
+    }
+    
+    int bytes_written = 0;
+    while (bytes_written < nbytes) {
+        int current_offset = offset + bytes_written;
+        int block_index = current_offset / BLOCKSIZE;
+        int block_offset = current_offset % BLOCKSIZE;
+        int bytes_to_write = BLOCKSIZE - block_offset;
+        
+        if (bytes_to_write > nbytes - bytes_written) {
+            bytes_to_write = nbytes - bytes_written;
+        }
+        char block_buf[BLOCKSIZE];
+        simplefs_readDataBlock(inode.direct_blocks[block_index], block_buf);
+        memcpy(block_buf + block_offset, buf + bytes_written, bytes_to_write);
+        simplefs_writeDataBlock(inode.direct_blocks[block_index], block_buf);
+        
+        bytes_written += bytes_to_write;
+    }
+    
+    if (offset + nbytes > inode.file_size) {
+        inode.file_size = offset + nbytes;
+    }
+    
+    simplefs_writeInode(inode_num, &inode);
+    
+    return 0;
 }
 
 
@@ -49,5 +243,21 @@ int simplefs_seek(int file_handle, int nseek){
     /*
 	   increase `file_handle` offset by `nseek`
 	*/
-    return -1;
+    if (file_handle < 0 || file_handle >= MAX_OPEN_FILES || 
+        file_handle_array[file_handle].inode_number == -1) {
+        return -1;
+    }
+    
+    int inode_num = file_handle_array[file_handle].inode_number;
+    int current_offset = file_handle_array[file_handle].offset;
+    
+    struct inode_t inode;
+    simplefs_readInode(inode_num, &inode);
+    int new_offset = current_offset + nseek;
+    if (new_offset < 0 || new_offset > inode.file_size) {
+        return -1;
+    }
+    file_handle_array[file_handle].offset = new_offset;
+    
+    return 0;
 }
